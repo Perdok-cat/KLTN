cd69a03f48b189382f32f07189b292502f0be6d7
phuc_leng_huu <mitech.huuphuc@gmail.com>
2026-03-05 08:14:32 +0700

    updateupdate

--- EDA_data.py
+++ EDA_data.py
@@ -4,6 +4,16 @@ import pandas as pd
 if __name__ == "__main__":
     df = pd.read_csv("data_2_3_2026.csv")
     
-    count = df.content.astype(str).str.contains("Không thể trích xuất nội dung", na=False).sum()
+    # count = df.content.astype(str).str.contains("Không thể trích xuất nội dung", na=False).sum()
 
-    print(count)
+    # mask = df.content.astype(str).str.contains("Không thể trích xuất nội dung", na=False) 
+
+    # df.drop(df.index[mask], inplace=True)
+    # df.reset_index(drop=True, inplace=True) 
+
+    mask_xau = df["content"].astype(str).str.contains("Không thể trích xuất nội dung", na=False)
+    mask_null = df["content"].isna()
+
+    df = df[~(mask_xau | mask_null)].reset_index(drop=True)
+
+    print(df.info())
